805af86ff64b832e8fd0dd999f5fb4295cbfd26c
Angel <angelsolaorbaiceta@gmail.com>
2024-12-16 08:27:30 +0100

    clangd

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -44,7 +42,14 @@ return {
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+	-- the key is the server name to configure
+	-- the value the configuration table
+	-- SEE: https://docs.astronvim.com/recipes/advanced_lsp/
+    	clangd = { 
+     		capabilities = { 
+        		offsetEncoding = "utf-8",
+      		},
+   	},
     },
     -- customize how language servers are attached
     handlers = {
