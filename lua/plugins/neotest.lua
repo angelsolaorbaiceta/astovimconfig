f5c222e5138eb1858ae454eb445ddea909917b87
Angel <angel.sola@wave.com>
2025-11-11 09:15:50 +0100

    Golang test runner

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -2,13 +2,27 @@ return {
   "nvim-neotest/neotest",
   dependencies = {
     "nvim-neotest/neotest-python",
+    "nvim-neotest/neotest-golang",
   },
   opts = function(_, opts)
     if not opts.adapters then opts.adapters = {} end
+
+    -- Python adapter
     table.insert(opts.adapters, require("neotest-python")({
       dap = { justMyCode = false },
       args = { "--log-level", "DEBUG", "-v" },
       runner = "pytest",
+    }))
+
+    -- Golang adapter
+    table.insert(opts.adapters, require("neotest-golang")({
+      go_test_args = {
+        "-v",
+        "-race",
+        "-count=1",
+        "-timeout=60s",
+      },
+      dap_go_enabled = true, -- Enable DAP debugging
     }))
   end,
 }
